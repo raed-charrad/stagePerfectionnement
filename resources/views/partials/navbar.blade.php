7096ba24685887fa1c1e81ab396c63b435499a65
Raed159753 <raed.charrad91@gmail.com>
2022-01-26 18:12:37 +0100

    .

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,38 @@
+
+<header role="banner">
+ 
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+       <div class="container">
+         <a class="navbar-brand" href="/">Alotan</a>
+         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+           <span class="navbar-toggler-icon"></span>
+         </button>
+
+         <div class="collapse navbar-collapse navbar-light " id="navbarsExample05" style="justify-content: flex-end; !important">
+           <ul class="navbar-nav ml-auto pl-lg-5 pl-0">
+             <li class="nav-item">
+               <a class="nav-link" href="/">Home</a>
+             </li>
+             <li class="nav-item dropdown">
+               <a class="nav-link dropdown-toggle" href="hairstyle.html" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Haircut</a>
+               <div class="dropdown-menu" aria-labelledby="dropdown04">
+                 <a class="dropdown-item" href="/hairstyle">Crew Cut</a>
+                 <a class="dropdown-item" href="/hairstyle">Regular Hair Cut</a>
+                 <a class="dropdown-item" href="/hairstyle">Shampoo + Cut</a>
+                 <a class="dropdown-item" href="/hairstyle">Beard Trim with Razor</a>
+                 <a class="dropdown-item" href="/hairstyle">Hair Color</a>
+               </div>
+
+             </li>
+             <li class="nav-item">
+               <a class="nav-link active" href="/about">About</a>
+             </li>
+             <li class="nav-item">
+               <a class="nav-link" href="/contact">Contact</a>
+             </li>
+           </ul>
+           
+         </div>
+       </div>
+     </nav>
+      </header>
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/index', function () {
     return view('index');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/hairstyle', function () {
+    return view('hairstyle');
 });
