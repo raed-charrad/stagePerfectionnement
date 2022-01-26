9dc5ae8a136ff2267300ba6429999a147bb7a2ae
Raed159753 <raed.charrad91@gmail.com>
2022-01-26 20:55:37 +0100

    front

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -107,10 +107,12 @@
         <div class="row justify-content-center align-items-center intro">
           <div class="col-md-8 text-center element-animate">
             <h2 class="mb-4"><span>Appoint a Haircut Today and</span> Get 25% discount</h2>
-            <p><a href="#" class="btn btn-black">Make an Appointment</a></p>
+            <p><button type="button" class="btn btn-black" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Make an Appointment</button></p>
           </div>
         </div>
       </div>
     </section>
     <!-- END section -->
+    @include('partials.modal')
+
 @endsection
--- resources/views/hairstyle.blade.php
+++ resources/views/hairstyle.blade.php
@@ -167,16 +167,18 @@
       </div>
     </section>
     <!-- END section -->
-
     <section class="section-cover cta" data-stellar-background-ratio="0.5" style="background-image: url(https://raw.githubusercontent.com/raed-charrad/stagePerfectionnement/master/resources/assets/images/big_image_2.jpg);">
       <div class="container">
         <div class="row justify-content-center align-items-center intro">
           <div class="col-md-8 text-center element-animate">
             <h2 class="mb-4"><span>Appoint a Haircut Today and</span> Get 25% discount</h2>
-            <p><a href="#" class="btn btn-black">Make an Appointment</a></p>
+            <p><button type="button" class="btn btn-black" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Make an Appointment</button></p>
+            <!-- Button trigger modal -->
           </div>
         </div>
       </div>
     </section>
+@include('partials.modal')
+
     <!-- END section -->
 @endsection
